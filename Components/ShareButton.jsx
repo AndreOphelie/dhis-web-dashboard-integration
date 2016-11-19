e306e2e7d17e4b8f1fc4e8c16405ac53f66a40ff
Julien Vedrenne <julien.vedrenne74@gmail.com>
2016-11-19 16:25:28 +0100

    Add Twitter publish with the right image

--- Components/ShareButton.jsx
+++ Components/ShareButton.jsx
@@ -31,6 +31,33 @@ function getBase64Image(img) {
     return dataURL.replace(/^data:image\/(png|jpg);base64,/, "");
 }
 
+/*function b64toBlob(b64Data, contentType, sliceSize) {
+    contentType = contentType || '';
+    sliceSize = sliceSize || 512;
+
+    var byteCharacters = atob(b64Data);
+    var byteArrays = [];
+
+    for (var offset = 0; offset < byteCharacters.length; offset += sliceSize) {
+        var slice = byteCharacters.slice(offset, offset + sliceSize);
+
+        var byteNumbers = new Array(slice.length);
+        for (var i = 0; i < slice.length; i++) {
+            byteNumbers[i] = slice.charCodeAt(i);
+        }
+
+        var byteArray = new Uint8Array(byteNumbers);
+
+        byteArrays.push(byteArray);
+    }
+
+    var blob = new Blob(byteArrays, {type: contentType});
+    return blob;
+}*/
+
+
+//Convert base64 into blob
+//cf http://stackoverflow.com/questions/16245767/creating-a-blob-from-a-base64-string-in-javascript
 function b64toBlob(b64Data, contentType, sliceSize) {
     contentType = contentType || '';
     sliceSize = sliceSize || 512;
@@ -129,11 +156,69 @@ class ShareButton extends React.Component {
     _confirm_publish(){
         console.log('STATE'+this.state.social);
         if(this.state.social == 'fb'){
-            console.log('FBBB');
+            console.log('Facebook Request');
             this._uploadFacebook();
         }
+        if(this.state.social == 'tw'){
+            console.log('Twitter Request');
+            this._uploadTwitter();
+        }
     }
-    _uploadFacebook(){
+    _uploadTwitter(){
+
+        var comment = this.state.comment;
+        var type = this.props.type;
+
+
+        const contentType = 'image/png';
+
+        var img = new Image();
+        img.src = "http://localhost:8082/api/" + this.props.type + "/" + this.props.id + "/data";
+
+        console.log(img);
+        var logo = "iVBORw0KGgoAAAANSUhEUgAAABAAAAAQCAYAAAAf8/9hAAAACXBIWXMAAAsTAAALEwEAmpwYAAAAIGNIUk0AAHolAACAgwAA+f8AAIDpAAB1MAAA6mAAADqYAAAXb5JfxUYAAAKfSURBVHjabJNNSFRRGIafc+6d8Y6OM42aTmpNauRgUVBRVEwZCC6kdtGqRRBtWkWLKGgXtghaRIsK3ES0L4pKCPpZREX2oxnVlOn4V4026Tjj3HvPPS2asRp64OODw8v3cXjfTxinrlOGBHSxzgPjxW4Cqvj+j7icy8AVIAIcK5YPuAMc/9+2fxEioWz3sFoozCrloVzVrLKLtud6XQiRKKlKcrPYo8AbhEiq+Xxba2MN7dEwtqsQCExT8moszfRMNmFUWwN4uhKI/z1gHikCai6/fefaRlqiYW6+HGEunQU00cYaejbGeJacjgym0hEjGBhC66UvhIEWlbPHN6yqo7UhzLUbz9mxup65C4dInTtIxPLRd/sFW1vqaaoNoRw1DawAQhI4Awzi6fjahjD97ybAUfTuWc+Je6+5l5zmTOc6mMnydOQbm2N1sGh3AZNArwR6Pa0fRGqD5BYdvk7MEoo3sbG1npXVFsv8JtviTdAWZWjyB8pRUOkHuAqcNYEpDQOmFJ2O8iCbpyfegRSCk53rlszZFKtjYCiFqzyElAAPgQkTuGgIcfR7JkeouRYCfrw/Li1RcD38oQCW5UMvFKCqog/YYgJ9QAbHPTKbt5cnNrVy980op+uCzOVshBCYhmR4LM2+DTFG0vNgGu+BS8AjUYqyFiLp5QttBza38flnjudPPoDt/k5ulUX37g6k43JncAwjGOhH626A0oA1wEfl6UVs10q0NxK2fGQLLkJAqMLHeGaBF5++KllVYQjIF+13SkFKAi2GFN+wfMOP36ZiZqWfppogrqeZTM+jXYURtO4XN8cAp/wWvgA5YMoIWiOelPtHZ7JMZBYywm/uNSorQOvRona0/Bb+ZhcgBRQMQ14CUsAtoAoolIt/DQBMqAUSa5wR2gAAAABJRU5ErkJggg==";
+
+
+
+
+        img.addEventListener('load', function () {
+            var image = getBase64Image(img);
+            console.log("couocuocuocuc");
+            console.log(image);
+            //var blob = b64toBlob(image, contentType);
+           // var name =  type + 'png';
+
+            // Initialize with your OAuth.io app public key
+            OAuth.initialize('SB6S4-dwB3azNlMTtoqSvhvLNv8');
+
+
+            OAuth.popup("twitter").then(function(result) {
+                console.log(result);
+                var data = new FormData();
+                data.append('status', comment);
+                data.append('media[]', b64toBlob(image), 'logo.png');
+
+                return result.post('/1.1/statuses/update_with_media.json', {
+                    data: data,
+                    cache:false,
+                    processData: false,
+                    contentType: false
+                });
+            }).done(function(data){
+                var str = JSON.stringify(data, null, 2);
+                //$('#result').html("Success\n" + str).show()
+                console.log("Success\n" + str);
+            }).fail(function(e){
+                var errorTxt = JSON.stringify(e, null, 2)
+                //$('#result').html("Error\n" + errorTxt).show()
+                console.log("Error\n" + errorTxt);
+            });
+
+
+            });
+    }
+
+            _uploadFacebook(){
 
         var comment = this.state.comment;
         var close = this._close();
